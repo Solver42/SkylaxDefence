7956ebd35320e898fbb22d6e3a01d8a2ecfd7f2b
Joe <joppeakerlund@outlook.com>
2014-08-24 21:00:08 +0200

    Removed duplicated direcory

--- 9/PoliceSearch/src/se/MinimalisticPerfectionTechnology/start/PoliceSearch.java
+++
@@ -1,9 +0,0 @@
-package se.MinimalisticPerfectionTechnology.start;
-
-public class PoliceSearch {
-
-	public static void main(String[] snopp) {
-		System.out.println("Processing...");
-		new SuperUI(snopp[0], snopp);
-	}
-}
