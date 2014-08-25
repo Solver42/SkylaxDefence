ae8415f70d627a3bb734a7c56d7127ee2e802a5b
Joe <joppeakerlund@outlook.com>
2014-08-25 19:12:59 +0200

    Sorry broh for all the commits ;-)

---
+++ PoliceSearch/PoliceSearch/src/se/MinimalisticPerfectionTechnology/start/PoliceSearch.java
@@ -0,0 +1,9 @@
+package se.MinimalisticPerfectionTechnology.start;
+
+public class PoliceSearch {
+
+	public static void main(String[] yeah) {
+		System.out.println("Processing...");
+		new PathPrinter(yeah[0], yeah);
+	}
+}
---
+++ uKeep/uKeep/src/se/Client/ClientMain.java
@@ -0,0 +1,10 @@
+package se.Client;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		new Client(args[0]);
+
+	}
+
+}
