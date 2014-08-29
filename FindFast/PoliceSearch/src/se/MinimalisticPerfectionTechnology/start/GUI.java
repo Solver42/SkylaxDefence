80ef3561db2e8b169fdc9dc54544c593d8973b0a
Joe <joppeakerlund@outlook.com>
2014-08-29 19:44:32 +0200

    Now u can double click the files to open them

--- FindFast/PoliceSearch/src/se/MinimalisticPerfectionTechnology/start/GUI.java
+++ FindFast/PoliceSearch/src/se/MinimalisticPerfectionTechnology/start/GUI.java
@@ -21,12 +21,14 @@ import java.awt.Dimension;
 import java.awt.SystemColor;
 import java.awt.Color;
 
-import javax.swing.DropMode;
-import javax.swing.JSplitPane;
+
+
+import java.awt.Desktop;
 import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class GUI {
 
@@ -34,7 +36,7 @@ public class GUI {
 	private JList list;
 	private PathPrinter pathPrinter = null;
 	private JTextArea txtEnterSearchWordwords;
-	private JList resultList;
+	private JList resultList_1;
 
 
 	private DefaultListModel model;
@@ -58,63 +60,83 @@ public class GUI {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	
-	
+
+
 	public void addItemToList(String str)
 	{
 		model.addElement(str);
 	}
 
 	private void initialize() {
-		
+
 		model = new DefaultListModel();
 		model.addElement("resultList");
-		
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 660, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("SearchFurther");
 		frame.setMinimumSize(new Dimension(560, 180));
 		frame.getContentPane().setLayout(new GridLayout(2, 0, 0, 0));
-				
-				JPanel panel_1 = new JPanel();
-				frame.getContentPane().add(panel_1);
-						panel_1.setLayout(new GridLayout(2, 1, 0, 0));
-				
-						txtEnterSearchWordwords = new JTextArea();
-						txtEnterSearchWordwords.setForeground(Color.DARK_GRAY);
-						panel_1.add(txtEnterSearchWordwords);
-						txtEnterSearchWordwords.setBackground(SystemColor.activeCaption);
-						txtEnterSearchWordwords.setText("Enter search word/words separated by comma...");
-						txtEnterSearchWordwords.setRows(3);
-						
-						JPanel panel_4 = new JPanel();
-						panel_1.add(panel_4);
-						panel_4.setBackground(SystemColor.activeCaption);
-						panel_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-						
-						txtEnterPathOf = new JTextField();
-						txtEnterPathOf.setText("Enter the path that you'd like to search trough...");
-						txtEnterPathOf.setForeground(Color.GREEN);
-						txtEnterPathOf.setBackground(Color.BLACK);
-						panel_4.add(txtEnterPathOf);
-						txtEnterPathOf.setColumns(40);
-						
-								JButton btnSearch = new JButton("Search");
-								panel_4.add(btnSearch);
-								btnSearch.setAction(action);
-												
-												JPanel panel_2 = new JPanel();
-												frame.getContentPane().add(panel_2);
-														panel_2.setLayout(new GridLayout(0, 1, 0, 0));
-												
-														JList resultList_1 = new JList(model);
-														resultList_1.setForeground(Color.DARK_GRAY);
-														JScrollPane scrollPane = new JScrollPane();
-														panel_2.add(scrollPane);
-														scrollPane.setViewportView(resultList_1);
-														resultList_1.setBackground(Color.WHITE);
-		
+
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(new GridLayout(2, 1, 0, 0));
+
+		txtEnterSearchWordwords = new JTextArea();
+		txtEnterSearchWordwords.setForeground(Color.DARK_GRAY);
+		panel_1.add(txtEnterSearchWordwords);
+		txtEnterSearchWordwords.setBackground(SystemColor.activeCaption);
+		txtEnterSearchWordwords.setText("Enter search word/words separated by comma...");
+		txtEnterSearchWordwords.setRows(3);
+
+		JPanel panel_4 = new JPanel();
+		panel_1.add(panel_4);
+		panel_4.setBackground(SystemColor.activeCaption);
+		panel_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		txtEnterPathOf = new JTextField();
+		txtEnterPathOf.setText("Enter the path that you'd like to search trough...");
+		txtEnterPathOf.setForeground(Color.GREEN);
+		txtEnterPathOf.setBackground(Color.BLACK);
+		panel_4.add(txtEnterPathOf);
+		txtEnterPathOf.setColumns(40);
+
+		JButton btnSearch = new JButton("Search");
+		panel_4.add(btnSearch);
+		btnSearch.setAction(action);
+
+		JPanel panel_2 = new JPanel();
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(new GridLayout(0, 1, 0, 0));
+
+		resultList_1 = new JList(model);
+		resultList_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if(e.getClickCount() == 2)
+				{
+
+					File file = new File(resultList_1.getSelectedValue().toString());
+					if(file.canExecute())
+					{
+						try {
+							Desktop.getDesktop().open(file);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		resultList_1.setForeground(Color.DARK_GRAY);
+		JScrollPane scrollPane = new JScrollPane();
+		panel_2.add(scrollPane);
+		scrollPane.setViewportView(resultList_1);
+		resultList_1.setBackground(Color.WHITE);
+
 	}
 
 	private class SwingAction extends AbstractAction {
